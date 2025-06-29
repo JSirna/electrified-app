87994768e3608abb9bb3ad78e94efe1cc590def9
JSirna <jms_2696@outlook.com>
2025-06-29 12:44:32 -0400

    add error checking

--- src/JSirna/character_design_api.js
+++ src/JSirna/character_design_api.js
@@ -1,54 +1,95 @@
-const baseURL = "http://localhost:5067/api";
+const baseURL = "hp://localhost:5067/api";
 // const URL = "http://localhost:5067/api/get-families";
 const button = document.getElementById("get-data");
 // const output = document.getElementById("results");
 const characterChart = new CharacterChart();
 
+
+const isValidUrl = (string) => {
+    try {
+        const newURL = new URL(string)
+        return newURL.protocol === "http:" || newURL.protocol === "https:";
+    } catch (error) {
+        console.error(string + " is not a valid URL: " + error.message);
+        return false;
+    }
+}
+
 const fetchCharacters = async () => {
     let data = {};
-    try {
-        const response = await fetch(`${baseURL}/get-characters`, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
+    const response = await fetch(`${baseURL}/get-characters`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+    if (!response.ok) {
+        console.log(response.status);
+        switch (response.status) {
+            case 404:
+                console.error("Resource not found");
+                break;
+            case 401:
+                console.error("Not authorized to access resource");
+                break;
+            default:
+                console.error("Error fetching resource");
+        }
+    }
+    else
         data = await response.json();
-        return data;
-    }
-    catch (error) {
-        console.error("Error fetching characters:", error.message);
-    }
+    return data;
 }
 
 const fetchCharactersFamilies = async () => {
     let data = {};
-    try {
-        const response = await fetch(`${baseURL}/get-families`, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
+    const response = await fetch(`${baseURL}/get-families`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+    if (!response.ok) {
+        switch (response.status) {
+            case 404:
+                console.error("Resource not found")
+                break;
+            case 401:
+                console.error("Not authorized to access resource")
+                break;
+            default:
+                console.log("Error fetching resource")
+        }
+    }
+    else
         data = await response.json();
-        return data;
-    }
-    catch (error) {
-        console.error("Error fetching characters:", error.message);
-    }
+    return data;
 }
 
 button.addEventListener('click', async () => {
-    try {
-        const fams = await fetchCharactersFamilies();
-        const chars = await fetchCharacters();
-        console.log("Characters:", chars);
-        console.log("Families:", fams);
-        parseData(chars);
-        console.log("Character Chart:", characterChart);
-    } catch (error) {
-        console.error("Error fetching data:", error.message);
+    // Try fetching the Character info on click
+    if (isValidUrl(baseURL)) {
+        console.log(isValidUrl(baseURL));
+        try {
+            const chars = await fetchCharacters();
+            console.log("Characters:", chars);
+            parseData(chars);
+            console.log("Character Chart:", characterChart);
+        } catch {
+            console.error("Failed to fetch character info.");
+        }
+
+        // Try fetching the Character family info on click
+        try {
+            const fams = await fetchCharactersFamilies();
+            console.log("Families:", fams);
+        } catch {
+            console.error("Failed to fetch character family info.");
+        }
     }
+    else
+        console.error(baseURL + " is not a valid URL.");
+    
 });
 
 const parseData = (listofObjects) => {
